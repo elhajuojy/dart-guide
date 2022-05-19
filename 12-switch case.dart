7e1f8a2f91da5c223f228037fb4980d9a3394f24
elhajuojy <souirimehdi311@gmail.com>
2022-05-20 00:01:43 +0100

    switch case

---
+++ 12-switch case.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  int price = 1000;
+
+  switch (price) {
+    case 100:
+      {
+        print("i have " + 1000.toString());
+      }
+      break;
+    case 2000:
+      {
+        print("i have " + 2000.toString());
+      }
+      break;
+    default:
+      {
+        print(" i have" + "nothing");
+      }
+  }
+}
