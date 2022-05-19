e13583de20244128750cd47a830600c69862fcbc
elhajuojy <souirimehdi311@gmail.com>
2022-05-19 23:19:46 +0100

    Logical Operators

---
+++ 9 - Logical Operators.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int a = 10;
+  int b = 20;
+
+  // *and -&& *or *not
+
+  //false and
+  print(a > 10 && b > 10);
+
+  //true  or
+  print(a > 10 || b > 10);
+
+  //true not
+  print(!(a > 10 || b > 10));
+}
