2d320f7d946ed4c84f50dedb07c35507af4c223c
elhajuojy <souirimehdi311@gmail.com>
2022-05-19 23:55:26 +0100

    if else statement

---
+++ 11 - if else statement.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int iphone = 6000;
+  int s20 = 3000;
+  int s10 = 1000;
+
+  int price = 3000;
+
+  if (price == s10) {
+    print("i will buy iphone");
+  } else if (s10 == price) {
+    print("i will buy s10");
+  } else {
+    print("i will buy s20");
+  }
+}
