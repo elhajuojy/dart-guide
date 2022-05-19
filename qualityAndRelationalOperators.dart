cab0a78c1752e7954e88682d3becae0f3361b81a
elhajuojy <souirimehdi311@gmail.com>
2022-05-19 23:05:00 +0100

    quality and relational operators

---
+++ qualityAndRelationalOperators.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  int a = 10;
+  int b = 10;
+
+  //true
+  print(a == b);
+
+  //false
+  print(a > b);
+
+  //true
+  print(a >= b);
+
+  //false (notequal)
+  print(a != b);
+}
