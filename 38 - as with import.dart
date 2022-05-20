24e482544125e2b451453de155c460b8d9c2e903
elhajuojy <souirimehdi311@gmail.com>
2022-05-20 21:04:14 +0100

    import as

---
+++ 38 - as with import.dart
@@ -0,0 +1,9 @@
+import 'dart:math' as math;
+import 'v.dart';
+
+void main(List<String> args) {
+  //import as for never use this name space until we call the as name;
+
+  print(math.sqrt(4));
+  print(subnmber(2, 4));
+}
