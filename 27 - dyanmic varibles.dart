77be9f16457ddd3c69f8fa5eaf3158b10c1fc603
elhajuojy <souirimehdi311@gmail.com>
2022-05-20 01:48:07 +0100

    dyanmic varibles Var

---
+++ 27 - dyanmic varibles.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  var name = "mehdi";
+  var age = 30;
+  var note = 3.3;
+  var isAlive = true;
+  var names = ["mehdi", "elhjuojy"];
+  var myMap = {"firstName": "mehdi", "lastName": "elhjuojy"};
+}
