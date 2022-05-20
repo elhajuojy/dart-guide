1fad0794ea39533f026ec89ef8062f16f270f9b9
elhajuojy <souirimehdi311@gmail.com>
2022-05-20 01:13:28 +0100

    List

---
+++ 20 - List.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  List names = ["mehdi", "houssam", "zineb", "ahmmed", "souad"];
+  print(names[1]); //houssam
+  print(names[0]);
+
+  List ListInsideList = [
+    [
+      ["mehdi", "eljuojy"],
+      "houssam",
+      "zineb",
+      "ahmed"
+    ],
+    ["elhjuojy", "jebbar", "belhaid", "bounacer"]
+  ];
+  print(ListInsideList[0][1]);
+}
