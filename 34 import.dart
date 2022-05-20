d787c280e24000f8dbbf7b55a94b5f27ea9279ff
elhajuojy <souirimehdi311@gmail.com>
2022-05-20 20:24:06 +0100

    import

---
+++ 34 import.dart
@@ -0,0 +1,9 @@
+import 'v.dart';
+
+// import '../w.dart';
+// import 's/v.dart';
+
+void main(List<String> args) {
+  //impot subnmber from v.dart
+  subnmber(30, 3);
+}
---
+++ v.dart
@@ -0,0 +1,4 @@
+subnmber(int a, int b) {
+  int c = a + b;
+  print(c);
+}
