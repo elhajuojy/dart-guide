987886b3621f1b9cb45fe0224d291b4b6a67dbf5
elhajuojy <souirimehdi311@gmail.com>
2022-05-21 12:07:48 +0100

    Collection

---
+++ 65 - Collection.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  List names = [];
+  names.add('mehdi');
+  names.add('mehdi');
+
+  Map MapName = new Map();
+  MapName.addAll({'name': 'mehdi', 'age': 20});
+  print(MapName);
+}
